eedcf6f165536fd2385066269adc5540a873a072
Martin Roa Villescas <mroavi@gmail.com>
2022-11-05 17:44:49 +0100

    Format bench_mmap.jl

--- benchmark/bench_mmap.jl
+++ benchmark/bench_mmap.jl
@@ -10,15 +10,15 @@ problem = read_uai_problem("Promedus_14")
 optimizer = TreeSA(ntrials = 1, niters = 2, βs = 1:0.1:40)
 
 # Does not marginalize any var
-mmap1 = MMAPModel(problem; marginalizedvertices = Int[], optimizer) 
+mmap1 = MMAPModel(problem; marginalizedvertices = Int[], optimizer)
 SUITE["mmap-1"] = @benchmarkable maximum_logp(mmap1)
 
 # Marginalizes all vars
-mmap2 = MMAPModel(problem; marginalizedvertices = collect(1:(problem.nvars)), optimizer) 
+mmap2 = MMAPModel(problem; marginalizedvertices = collect(1:(problem.nvars)), optimizer)
 SUITE["mmap-2"] = @benchmarkable maximum_logp(mmap2)
 
 # Does not optimize over open vertices
-mmap3 = MMAPModel(problem; marginalizedvertices = [2, 4, 6], optimizer) 
+mmap3 = MMAPModel(problem; marginalizedvertices = [2, 4, 6], optimizer)
 SUITE["mmap-3"] = @benchmarkable most_probable_config(mmap3)
 
 end  # module
