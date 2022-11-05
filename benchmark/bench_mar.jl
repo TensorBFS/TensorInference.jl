29606cbc5688148e013e1f436ac5ce2fa32c1a6e
Martin Roa Villescas <mroavi@gmail.com>
2022-11-05 16:53:19 +0100

    Disable mar CUDA benchmark

--- benchmark/bench_mar.jl
+++ benchmark/bench_mar.jl
@@ -3,8 +3,8 @@ module BenchMar
 using BenchmarkTools
 using TensorInference
 using Artifacts
-using CUDA
-CUDA.allowscalar(false)
+# using CUDA
+# CUDA.allowscalar(false)
 
 const SUITE = BenchmarkGroup()
 
@@ -14,9 +14,9 @@ optimizer = TreeSA(ntrials = 1, niters = 5, βs = 0.1:0.1:100)
 tn1 = TensorNetworkModel(problem; optimizer)
 SUITE["mar"] = @benchmarkable marginals(tn1)
 
-optimizer = TreeSA(ntrials = 1, niters = 2, βs = 1:0.1:40)
-tn2 = TensorNetworkModel(problem; optimizer)
-SUITE["mar-cuda"] = @benchmarkable marginals(tn2; usecuda = true)
+# optimizer = TreeSA(ntrials = 1, niters = 2, βs = 1:0.1:40)
+# tn2 = TensorNetworkModel(problem; optimizer)
+# SUITE["mar-cuda"] = @benchmarkable marginals(tn2; usecuda = true)
 
 end  # module
 BenchMar.SUITE
