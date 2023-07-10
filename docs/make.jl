c27507268228d3ebb6a33701e2be2f0879f03baf
GiggleLiu <cacate0129@gmail.com>
2023-07-10 23:37:05 +0800

    initial

--- docs/make.jl
+++ docs/make.jl
@@ -1,10 +1,20 @@
 using TensorInference
-using Documenter
+using TensorInference: OMEinsum, OMEinsumContractionOrders
+using Documenter, Literate
+
+# Literate
+for each in readdir(pkgdir(TensorInference, "examples"))
+    input_file = pkgdir(TensorInference, "examples", each)
+    endswith(input_file, ".jl") || continue
+    @info "building" input_file
+    output_dir = pkgdir(TensorInference, "docs", "src", "generated")
+    Literate.markdown(input_file, output_dir; name=each[1:end-3], execute=false)
+end
 
 DocMeta.setdocmeta!(TensorInference, :DocTestSetup, :(using TensorInference); recursive=true)
 
 makedocs(;
-    modules=[TensorInference],
+    modules=[TensorInference, OMEinsumContractionOrders],
     authors="Jin-Guo Liu, Martin Roa Villescas",
     repo="https://github.com/TensorBFS/TensorInference.jl/blob/{commit}{path}#{line}",
     sitename="TensorInference.jl",
@@ -16,7 +26,13 @@ makedocs(;
     ),
     pages=[
         "Home" => "index.md",
+        "Examples" => [
+            "Asia network" => "generated/asia.md",
+           ],
+        "Performance Tips" => "performance.md",
+        "References" => "ref.md",
     ],
+    doctest = false,
 )
 
 deploydocs(;
---
+++ docs/src/ref.md
@@ -0,0 +1,23 @@
+# References
+
+## TensorInference
+```@autodocs
+Modules = [TensorInference]
+Order   = [:function, :type]
+```
+
+## Tensor Network
+```@docs
+optimize_code
+getixsv
+getiyv
+contraction_complexity
+estimate_memory
+@ein_str
+GreedyMethod
+TreeSA
+SABipartite
+KaHyParBipartite
+MergeVectors
+MergeGreedy
+```
