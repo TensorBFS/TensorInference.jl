a51fff9e0f2a62e9c36c680d3f811960172808d9
Documenter.jl <documenter@juliadocs.github.io>
2023-07-11 19:23:35 +0000

    build based on 57f5068

---
+++ dev/generated/asia/main.jl
@@ -0,0 +1,38 @@
+using TensorInference
+
+# Load the model that detailed in the README and `asia.uai`.
+instance = read_instance(pkgdir(TensorInference, "examples", "asia", "asia.uai"))
+tnet = TensorNetworkModel(instance)
+
+# Get the probabilities (PR)
+probability(tnet)
+
+# Get the marginal probabilities (MAR)
+marginals(tnet) .|> first
+
+# The corresponding variables are
+get_vars(tnet)
+
+# Set the evidence variables "X-ray" (7) to be positive.
+set_evidence!(instance, 7=>0)
+
+# Since the evidence variable may change the contraction order, we re-compute the tensor network.
+tnet = TensorNetworkModel(instance)
+
+# Get the maximum log-probabilities (MAP)
+maximum_logp(tnet)
+
+# To sample from the probability model
+sample(tnet, 10)
+
+# Get not only the maximum log-probability, but also the most probable conifguration
+# In the most probable configuration, the most probable one is the patient smoke (3) and has lung cancer (4)
+logp, cfg = most_probable_config(tnet)
+
+# Get the maximum log-probabilities (MMAP)
+# To get the probability of lung cancer, we need to marginalize out other variables.
+mmap = MMAPModel(instance; queryvars=[4,7])
+# We get the most probable configurations on [4, 7]
+most_probable_config(mmap)
+# The total probability of having lung cancer is roughly half.
+log_probability(mmap, [1, 0]), log_probability(mmap, [0, 0])
