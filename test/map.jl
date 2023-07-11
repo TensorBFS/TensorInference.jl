63ee8690e3718130f8f184b389ffb6421cf1bd1a
Martin Roa Villescas <mroavi@gmail.com>
2023-07-11 03:58:16 +0200

    Add MAP UAI reference solution comparison test

--- test/map.jl
+++ test/map.jl
@@ -14,3 +14,12 @@ using TensorInference
     @test log_probability(tn, config) ≈ logp
     @test maximum_logp(tn)[] ≈ logp
 end
+
+@testset "UAI Reference Solution Comparison" begin
+    model_filepath, evidence_filepath, solution_filepath = get_instance_filepaths("Promedas_70", "MAP")
+    instance = read_instance(model_filepath; evidence_filepath, solution_filepath)
+    ref_sol = read_solution_file(solution_filepath)[2:end]
+    tn = TensorNetworkModel(instance; optimizer = TreeSA(ntrials = 1, niters = 5, βs = 0.1:0.1:100))
+    _, ti_sol = most_probable_config(tn)
+    @test ti_sol == ref_sol
+end
