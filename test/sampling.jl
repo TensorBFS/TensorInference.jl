7bb8785f46d3bf478a56b83a102f909aac91b8c4
GiggleLiu <cacate0129@gmail.com>
2024-06-03 09:45:10 +0800

    update sampling method

--- test/sampling.jl
+++ test/sampling.jl
@@ -1,4 +1,4 @@
-using TensorInference, Test
+using TensorInference, Test, StatsBase, OMEinsum, LinearAlgebra
 
 @testset "sampling" begin
     model = TensorInference.read_model_from_string("""MARKOV
@@ -48,7 +48,7 @@ using TensorInference, Test
 """)
     n = 10000
     tnet = TensorNetworkModel(model)
-    samples = sample(tnet, n)
+    samples = TensorInference.sample(tnet, n)
     mars = marginals(tnet)
     mars_sample = [count(s->s[k]==(1), samples) for k=1:8] ./ n
     @test isapprox([mars[[i]][2] for i=1:8], mars_sample, atol=0.05)
@@ -59,4 +59,17 @@ using TensorInference, Test
     mars = marginals(tnet)
     mars_sample = [count(s->s[k]==(0), samples) for k=1:8] ./ n
     @test isapprox([[mars[[i]][1] for i=1:6]..., mars[[8]][1]], [mars_sample[1:6]..., mars_sample[8]], atol=0.05)
+end
+
+@testset "mps" begin
+    T = ComplexF64
+    A, B, C, D = randn(T, 2, 2, 2), randn(T, 2, 2, 2), randn(T, 2, 2, 2), randn(T, 2, 2, 1)
+    rawket = ein"aib,bjc,ckd,dlm->ijkl"
+    ket = optimize_code(rawket, uniformsize(rawket, 10), GreedyMethod())
+    p = vec(ket(A, B, C, D))
+    s1 = StatsBase.sample(0:15, Weights(abs2.(p)), 1000)
+    @test length(s1) == 1000
+    Z = (raw=ein"aib,bjc,ckd,dlm,αiβ,βjγ,γkδ,δlε->"; optimize_code(raw, uniformsize(raw, 10), GreedyMethod()))
+    tn = TensorNetworkModel(uniquelabels(Z), Z, [A, B, C, D, conj(A), conj(B), conj(C), conj(D)], Dict{Char, Int}(), Vector{Char}[])
+    TensorInference.sample(tn, 1000)
 end
